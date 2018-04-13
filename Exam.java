a80082eab49dbdb823f05fd66cf969fef86cc9e7
Tzket-Death <melyafi@gmail.com>
2018-04-13 10:19:18 -0500

    new code

--- Exam.java
+++ Exam.java
@@ -3,286 +3,307 @@ import java.util.*;
 
 public class Exam
 {
- private ArrayList<Question> questions;
- private String text;
- 
- public Exam()
- {
-  questions = new ArrayList<Question>();
-  return;
- }
- 
- public Exam(String Text)
- {
-  questions = new ArrayList<Question>();
-  text = Text;
-  return;
- }
- 
- public Exam(Scanner scanner)
- {
-  questions = new ArrayList<Question>();
-  String line = scanner.nextLine();
-  text = line;
-  while(scanner.hasNextLine())
+  private ArrayList<Question> questions;
+  private String text;
+  
+  public Exam()
   {
-   String line2 = scanner.nextLine();
-   if(!line2.equals(""))
-   {
-    if(line2.equals("SAQuestion"))
-    {
-     SAQuestion question = new SAQuestion(scanner);
-     questions.add(question);
-    }
-    else if(line2.equals("MCSAQuestion"))
-    {
-     MCSAQuestion question = new MCSAQuestion(scanner);
-     questions.add(question);
-    }
-    else if(line2.equals("MCMAQuestion"))
-    {
-     MCMAQuestion question = new MCMAQuestion(scanner);
-     questions.add(question);
-    }
-    else if(line2.equals("NumQuestion"))
-    {
-     NumQuestion question = new NumQuestion(scanner);
-     questions.add(question);
-    }
-    else
-    {
-     //do nothing, program doesn't support anything else
-    }
-   }
+    questions = new ArrayList<Question>();
+    return;
   }
   
-  return;
- }
- 
+  public Exam(String Text)
+  {
+    questions = new ArrayList<Question>();
+    text = Text;
+    return;
+  }
+  
+  public Exam(Scanner scanner)
+  {
+    questions = new ArrayList<Question>();
+    String line = scanner.nextLine();
+    text = line;
+    while(scanner.hasNextLine())
+    {
+      String line2 = scanner.nextLine();
+      if(!line2.equals(""))
+      {
+        if(line2.equals("SAQuestion"))
+        {
+          SAQuestion question = new SAQuestion(scanner);
+          questions.add(question);
+        }
+        else if(line2.equals("MCSAQuestion"))
+        {
+          MCSAQuestion question = new MCSAQuestion(scanner);
+          questions.add(question);
+        }
+        else if(line2.equals("MCMAQuestion"))
+        {
+          MCMAQuestion question = new MCMAQuestion(scanner);
+          questions.add(question);
+        }
+        else if(line2.equals("NumQuestion"))
+        {
+          NumQuestion question = new NumQuestion(scanner);
+          questions.add(question);
+        }
+        else
+        {
+          //do nothing, program doesn't support anything else
+        }
+      }
+    }
+    
+    return;
+  }
+  
   public void nameExam(String Text)
- {
-  text = Text;
- }
-  
- public String getExamName(){
-  return text; 
- } 
- public void print()
- {
-  System.out.println("\n" + text + "\n");
-  
-  int i = 1;
-  for(Question q : questions)
   {
-   System.out.print(i + ".) ");
-   q.print();
-   System.out.println();
-   i++;
+    text = Text;
   }
   
-  return;
- }
- 
- public void addQuestion(Question question)
- {
-  questions.add(question);
-  return;
- }
- 
- public void remove(int num)
- {
-  if(num < 1 || num > questions.size())
+  public String getExamName(){
+    return text; 
+  } 
+  public void print()
   {
-   System.out.println("\nInvalid question number...");
-   return;
-  }
-  else
-  {
-   questions.remove(num-1);
-   System.out.println("\nQuestion '" + num + "' removed...");
-   return;
-  }
- }
- 
- public void reorderQuestions()
- {
-  Collections.shuffle(questions);
-  return;
- }
- 
- public void reorderMCAnswers(int position)
- {
-  MCQuestion mcq;
-  
-  if(position < 0)
-  {
-   for(Question q : questions)
-   {
-    if(q instanceof MCQuestion)
+    System.out.println("\n" + text + "\n");
+    
+    int i = 1;
+    for(Question q : questions)
     {
-     mcq = (MCQuestion)q;
-     mcq.reorderAnswers();
+      System.out.print(i + ".) ");
+      q.print();
+      System.out.println();
+      i++;
     }
-   }
-  }
-  else
-  {
-   int i = 1;
-   for(Question q : questions)
-   {
-    if(i == position)
-    {
-     if(q instanceof MCQuestion)
-     {
-      mcq = (MCQuestion)q;
-      mcq.reorderAnswers();
-     }
-    }
-    i++;
-   }
+    
+    return;
   }
   
-  return;
- }
- 
- public void getAnswerFromStudent(int position)
- {
-  SAQuestion saq;
-  MCSAQuestion mcsaq;
-  MCMAQuestion mcmaq;
-  
-  int i = 1;
-  for(Question q : questions)
+  public void addQuestion(Question question)
   {
-   if(i == position)
-   {
-    if(q instanceof SAQuestion)
+    questions.add(question);
+    return;
+  }
+  
+  public void remove(int num)
+  {
+    if(num < 1 || num > questions.size())
     {
-     saq = (SAQuestion)q;
-     saq.getAnswerFromStudent();
-    }
-    else if(q instanceof MCSAQuestion)
-    {
-     mcsaq = (MCSAQuestion)q;
-     mcsaq.getAnswerFromStudent();
+      System.out.println("\nInvalid question number...");
+      return;
     }
     else
     {
-     mcmaq = (MCMAQuestion)q;
-     mcmaq.getAnswerFromStudent();
+      questions.remove(num-1);
+      System.out.println("\nQuestion '" + num + "' removed...");
+      return;
     }
-   }
-   i++;
   }
   
-  return;
- }
- 
- public double getValue()
- {
-  double score = 0.0;
-  
-  for(Question q : questions)
+  public void reorderQuestions()
   {
-   score += q.getValue();
+    Collections.shuffle(questions);
+    return;
   }
   
-  return score;
- }
- 
- public void reportQuestionValues()
- {
-  int i = 1;
-  double score = 0.0;
-  double totalScore = 0.0;
-  
-  System.out.println("The value of each question on the test and the overall test value is as follows:\n");
-  
-  for(Question q : questions)
+  public void reorderMCAnswers(int position)
   {
-   score = q.getMaxValue();
-   System.out.println("Question " + i + " is worth " + score + " points.");
-   totalScore += score;
-   i++;
+    MCQuestion mcq;
+    
+    if(position < 0)
+    {
+      for(Question q : questions)
+      {
+        if(q instanceof MCQuestion)
+        {
+          mcq = (MCQuestion)q;
+          mcq.reorderAnswers();
+        }
+      }
+    }
+    else
+    {
+      int i = 1;
+      for(Question q : questions)
+      {
+        if(i == position)
+        {
+          if(q instanceof MCQuestion)
+          {
+            mcq = (MCQuestion)q;
+            mcq.reorderAnswers();
+          }
+        }
+        i++;
+      }
+    }
+    
+    return;
   }
   
-  System.out.println("The total value of the test is " + totalScore + " points.");
-  return;
- }
- 
- public int getNumQuestions()
- {
-  return questions.size();
- }
- 
- public void save(PrintWriter p)
- {
-  p.println(text);
-  
-  for (int i=0; i < questions.size(); i++)
+  public void getAnswerFromStudent(int position)
   {
-   if(questions.get(i) instanceof MCSAQuestion)
-   {
-    p.println("MCSAQuestion");
-    questions.get(i).save(p);
-   }
-   else if(questions.get(i) instanceof MCMAQuestion)
-   {
-    p.println("MCMAQuestion");
-    questions.get(i).save(p);
-   }
-   else if(questions.get(i) instanceof SAQuestion)
-   {
-    p.println("SAQuestion");
-    questions.get(i).save(p);
-   }
-   else if(questions.get(i) instanceof NumQuestion)
-   {
-    p.println("NumQuestion");
-    questions.get(i).save(p);
-   }
-   else
-   {
-    //shouldn't be anything else
-   }
-   
+    SAQuestion saq;
+    MCSAQuestion mcsaq;
+    MCMAQuestion mcmaq;
+    NumQuestion nq;
+    
+    int i = 1;
+    for(Question q : questions)
+    {
+      if(i == position)
+      {
+        if(q instanceof SAQuestion)
+        {
+          saq = (SAQuestion)q;
+          saq.getAnswerFromStudent();
+        }
+        else if(q instanceof MCSAQuestion)
+        {
+          mcsaq = (MCSAQuestion)q;
+          mcsaq.getAnswerFromStudent();
+        }
+        else if(q instanceof MCMAQuestion)
+        {
+          mcmaq = (MCMAQuestion)q;
+          mcmaq.getAnswerFromStudent();
+        }
+        else
+        {
+          nq = (NumQuestion)q;
+          nq.getAnswerFromStudent();
+        }
+      }
+      i++;
+    }
+    
+    return;
   }
   
-  return;
- }
- 
- public void saveStudentAnswers(PrintWriter p)
- {
-  p.println(text);
-  for (int i=0; i < questions.size(); i++)
+  public double getValue()
   {
-   if(questions.get(i) instanceof MCSAQuestion)
-   {
-    p.println("MCSAQuestion");
-    questions.get(i).saveStudentAnswers(p);
-   }
-   if(questions.get(i) instanceof MCMAQuestion)
-   {
-    p.println("MCMAQuestion");
-    questions.get(i).saveStudentAnswers(p);
-   }
-   if(questions.get(i) instanceof SAQuestion)
-   {
-    p.println("SAQuestion");
-    questions.get(i).saveStudentAnswers(p);
-   }
-   if(questions.get(i) instanceof NumQuestion)
-   {
-    p.println("NumQuestion");
-    questions.get(i).saveStudentAnswers(p);
-   }
+    double score = 0.0;
+    
+    for(Question q : questions)
+    {
+      score += q.getValue();
+    }
+    
+    return score;
   }
-  return;
- }
- 
- public void restoreStudentAnswers(Scanner scanner)
- {
-  return;
- }
- 
+  
+  public void reportQuestionValues()
+  {
+    int i = 1;
+    double score = 0.0;
+    double totalScore = 0.0;
+    
+    System.out.println("The value of each question on the test and the overall test value is as follows:\n");
+    
+    for(Question q : questions)
+    {
+      score = q.getMaxValue();
+      //System.out.println("Question " + i + " is worth " + score + " points.");
+      System.out.println("Question " + i + ". Total points you scored:" + q.getValue() + "\n Total points possible:" + score);
+      totalScore += score;
+      i++;
+    }
+    
+    System.out.println("The total value of the test is " + totalScore + " points.");
+    return;
+  }
+  
+  public int getNumQuestions()
+  {
+    return questions.size();
+  }
+  
+  public void save(PrintWriter p)
+  {
+    p.println(text);
+    
+    for (int i=0; i < questions.size(); i++)
+    {
+      if(questions.get(i) instanceof MCSAQuestion)
+      {
+        p.println("MCSAQuestion");
+        questions.get(i).save(p);
+      }
+      else if(questions.get(i) instanceof MCMAQuestion)
+      {
+        p.println("MCMAQuestion");
+        questions.get(i).save(p);
+      }
+      else if(questions.get(i) instanceof SAQuestion)
+      {
+        p.println("SAQuestion");
+        questions.get(i).save(p);
+      }
+      else if(questions.get(i) instanceof NumQuestion)
+      {
+        p.println("NumQuestion");
+        questions.get(i).save(p);
+      }
+      else
+      {
+        //shouldn't be anything else
+      }
+      
+    }
+    
+    return;
+  }
+  
+  public void saveStudentAnswers(PrintWriter p)
+  {
+    p.println(text);
+    for (int i=0; i < questions.size(); i++)
+    {
+      if(questions.get(i) instanceof MCSAQuestion)
+      {
+        questions.get(i).saveStudentAnswers(p);
+      }
+      if(questions.get(i) instanceof MCMAQuestion)
+      {
+        questions.get(i).saveStudentAnswers(p);
+      }
+      if(questions.get(i) instanceof SAQuestion)
+      {
+        questions.get(i).saveStudentAnswers(p);
+      }
+      if(questions.get(i) instanceof NumQuestion)
+      {
+        questions.get(i).saveStudentAnswers(p);
+      }
+    }
+    return;
+  }
+  
+  public void restoreStudentAnswers(Scanner scanner){
+    for (int i=0; i < questions.size(); i++)
+    {
+      if(questions.get(i) instanceof MCSAQuestion)
+      {
+        questions.get(i).restoreStudentAnswers(scanner);
+      }
+      if(questions.get(i) instanceof MCMAQuestion)
+      {
+        questions.get(i).restoreStudentAnswers(scanner);
+      }
+      if(questions.get(i) instanceof SAQuestion)
+      {
+        questions.get(i).restoreStudentAnswers(scanner);
+      }
+      if(questions.get(i) instanceof NumQuestion)
+      {
+        questions.get(i).restoreStudentAnswers(scanner);
+      }
+    }
+    return;
+  }
+  
 }
--- ExamGrader.java
+++ ExamGrader.java
@@ -1,79 +1,44 @@
 import java.io.*;
 import java.util.*;
 
-public class ExamGrader{
- public static void main(String[] args){
-   int action = 0;
-  
-  int count = 0;
-  
-  int inputCheck = 0;
-  
-  int printCheck = 0;
-  
-  Scanner s = ScannerFactory.getKeyboardScanner();
-  
-  String input = s.nextLine();
-
-  
-  Exam exam = new Exam();
-  //LOOP
-  while(action != 7) {
-  inputCheck = 0;
-   if(action == 1) {
-    System.out.println("\nAction 'LOAD EXAM' selected...\n");
+public class ExamGrader
+{
+  public static void main(String[] args) throws IOException{
+    System.out.println();
+    int uploadCheck = 0;
     
-    try {
-     System.out.print("Enter the file name with extension : ");
-
-     Scanner scanner = ScannerFactory.getKeyboardScanner();
-
-     File file = new File(scanner.nextLine());
-
-     scanner = new Scanner(file);
-   
-     exam = new Exam(scanner);
-     
-     System.out.println("\nExam CREATED...");
-     
-     printCheck = 1;
-    }
-    catch(Exception ex)
-    {
-     System.out.println("SOMETHING WENT WRONG WITH THE FILE...");
+    int examCheck = 0;
+    
+    Scanner scan = ScannerFactory.getKeyboardScanner();
+    System.out.print("Enter the exam name with extension : ");
+    String inputExam = scan.nextLine();
+    
+    System.out.print("Enter the answer name with extension : ");
+    String inputAnswer = scan.nextLine();
+    
+    File fileAnswer = new File(inputAnswer);
+    Scanner scannerAnswer = new Scanner(fileAnswer);
+    
+    List<String> list = new ArrayList<String>();
+    while (scannerAnswer.hasNextLine()) {
+      list.add(scannerAnswer.nextLine());
     }
     
-   }
-   else if(action == 5)
-   {
-    System.out.println("\nAction 'PRINT EXAM' selected...\n");
+    System.out.println(list.get(1));
+    if (inputExam == null || inputExam.isEmpty()){
+         inputExam = list.get(1);
+    }
+    else if (!list.get(1).equals(inputExam)){
+      System.out.println("This exam does not match with these answers");
+      return;
+    }
     
-    if(printCheck == 1)
-     exam.print();
-    else
-     System.out.println("Exam not created yet, please try again.");
+    Exam exam = new Exam();
+    File fileExam = new File(inputExam);
+    Scanner scannerExam = new Scanner(fileExam);
+    exam = new Exam(scannerExam);
     
-   }
-   else if(action == 7)
-   {
-    System.out.println("\nAction 'QUIT' selected...\n");
-    
-    System.out.println("Quitting...");
-   }
-   else
-   {
-    System.out.println("\nSorry... Invalid action selected.");
-   }
-   
-   System.out.print("\nPlease enter another action to take: ");
-   
-   s = ScannerFactory.getKeyboardScanner();
-   
-   input = s.nextLine();
-  
-  System.out.println("DONE...");
-  
-  return;
- }
- }
+    exam.restoreStudentAnswers(scannerAnswer);
+    exam.reportQuestionValues();
+  }
 }
